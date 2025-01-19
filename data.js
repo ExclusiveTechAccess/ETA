1830194b85e8c24542b717cdfe94db8820b9f960
ExclusiveTechAccess <courseleaker9@gmail.com>
2025-01-20 01:37:36 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const allData = [
     {
-        chanel_image: "images/one.jpg",
+        chanel_image: "one.jpg",
         chanel_name: "ETA Main",
         chanel_desc:"An Official & Main Channel Of ExclusiveTechAccess...",
         chanel_owner:"Subhan Hassan",
@@ -8,7 +8,7 @@ const allData = [
         chanel_statys:"Active", chanel_link:`https://whatsapp.com/channel/0029Vb09gVg1t90cN4NGZ340`,
     },
        {
-        chanel_image: "images/two.jpg",
+        chanel_image: "two.jpg",
         chanel_name: "ETA Courses ",
         chanel_desc:"A Place Where You get Each Paid Course.... 100% For Free",
         chanel_owner:"Subhan Hassan",
@@ -16,7 +16,7 @@ const allData = [
         chanel_statys:"Active",     chanel_link:`https://whatsapp.com/channel/0029VadLkge8aKvEDicYat3J`,
     },
        {
-        chanel_image: "images/three.jpg",
+        chanel_image: "three.jpg",
         chanel_name: "ETA Accounts",
         chanel_desc:"A Place Where You get Each Paid Platform Premium Account...... 100% For Free",
         chanel_owner:"Subhan Hassan",
@@ -24,7 +24,7 @@ const allData = [
         chanel_statys:"Active", chanel_link:`https://whatsapp.com/channel/0029VawT4SlF6smtYD9O8W0Q`,
     },
        {
-        chanel_image: "images/four.jpg",
+        chanel_image: "four.jpg",
         chanel_name: "ETA Backup",
         chanel_desc:"An Backup Channel For Exclusive Tech Access (ETA)",
         chanel_owner:"Subhan Hassan",
@@ -32,7 +32,7 @@ const allData = [
         chanel_statys:"Active",   chanel_link:`https://whatsapp.com/channel/0029Vauud0lL7UVWdOUjnl05`,
     },
        {
-        chanel_image: "images/five.jpg",
+        chanel_image: "five.jpg",
         chanel_name: "ETA Chat GC",
         chanel_desc:"A Group Where You Can Discuss Anything Related To Data Leaking Field and Gain knowledge",
         chanel_owner:"Subhan Hassan",
